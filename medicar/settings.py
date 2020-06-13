d81a1446fb3c89d5c0b96fbcc009ae4034890457
danizavtz <daniellucena@yahoo.com.br>
2020-06-13 00:52:58 -0300

    Setando authenticação

--- medicar/settings.py
+++ medicar/settings.py
@@ -26,6 +26,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',
     'medico',
     'agenda',
 ]
@@ -89,6 +90,15 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+               'rest_framework.authentication.TokenAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES':(
+                'rest_framework.permissions.IsAuthenticated',
+    ),
+}
 
 
 # Internationalization
--- medicar/urls.py
+++ medicar/urls.py
@@ -1,19 +1,14 @@
-# from django.contrib import admin
-# from django.urls import path
-
-# urlpatterns = [
-#     path('admin/', admin.site.urls),
-# ]
-
 from django.urls import include, path
 from rest_framework import routers
-from quickstart import views
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.authtoken import views as tokenview
+from quickstart import views as rfviews
 from medico import views as mview
 from agenda import views as aview
 
 router = routers.DefaultRouter()
-router.register(r'users', views.UserViewSet)
-router.register(r'groups', views.GroupViewSet)
+router.register(r'users', rfviews.UserViewSet)
+router.register(r'groups', rfviews.GroupViewSet)
 router.register(r'medicos', mview.MedicoViewSet, basename='medicos')
 router.register(r'especialidades', mview.EspecialidadeViewSet, basename='especialidades')
 router.register(r'consultas', aview.ConsultaViewSet, basename='consultas')
@@ -23,5 +18,6 @@ router.register(r'agendas', aview.AgendaViewSet, basename='agendas')
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('login/', tokenview.obtain_auth_token, name='api-tokn-auth')
 ]
